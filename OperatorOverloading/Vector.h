edb57a0e42c3814642a7ed7e29595525ab9b3da4
Cvetomir Denchev <cvetomirdenchev@gmail.com>
2018-02-16 09:35:41 +0200

    First commit. Added gitignore file and some Operator overloading project (taken mostly when taking TheCherno's C++ course).

---
+++ OperatorOverloading/Vector.h
@@ -0,0 +1,56 @@
+#ifndef VECTOR_ONCE
+#define VECTOR_ONCE
+
+struct Vector
+{
+    float x, y;
+
+    Vector(float x, float y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    Vector() {
+        this->x = 1.0f;
+        this->y = 0.0f;
+    }
+
+    Vector Add(const Vector &other) const
+    {
+        return *this + other;
+    }
+
+    Vector Multiply(const Vector &other) const {
+        return *this * other;
+    }
+
+    Vector operator+(const Vector &other) const {
+        return Vector(x + other.x, y + other.y);
+    }
+
+    Vector operator*(const Vector &other) const {
+        return Vector(x * other.x, y * other.y);
+    }
+
+    bool operator==(const Vector &other) const {
+        return x == other.x && y == other.y;
+    }
+
+    bool operator!=(const Vector &other) const {
+        return !(*this == other);
+        //return !operator==(other);
+    }
+
+    Vector* getVector() const {
+        const Vector* v = this;
+    }
+
+    static int VectorInit(){};
+
+    ~Vector() {
+        std::cout << "Vector Destroyed!!!" << std::endl;
+    }
+
+};
+
+#endif
---
+++ OperatorOverloading/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "Vector.h"
+#include <memory>
+
+using namespace std;
+
+int main()
+{
+
+    {
+        Vector position(4.0f, 4.0f);
+    }
+
+    cout << "After init" << endl;
+
+    Vector speed(0.5f, 1.5f);
+    Vector powerup(1.1f, 1.0f);
+
+    Vector init(1.1f, 1.0f);
+    Vector* memLeak = new Vector(0.0f, 0.0f);
+    Vector* strange = new Vector();
+
+    const Vector* i = &init;
+    i = &powerup;
+
+    bool areEqual = init == powerup;
+
+    cout << areEqual << endl;
+    cout << strange->x << endl;
+
+    delete memLeak;
+    delete strange;
+
+    int(*initVar)() = Vector::VectorInit;
+
+    //unique pointer
+    //shared pointer
+
+    std::shared_ptr<Vector> v1;
+    {
+        std::shared_ptr<Vector> shared = std::make_shared<Vector>();
+        std::weak_ptr<Vector> weakPtr =
+        v1 = shared;
+    }
+
+}
