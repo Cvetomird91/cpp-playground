47a6f3d353682f6d3167b6b8f6e8b16ec7f8e5ff
Cvetomir Denchev <cvetomirdenchev@gmail.com>
2018-03-15 12:01:40 +0200

    added regex to match *.txt files in BoostFileSystem

--- BoostFileSystem/main.cpp
+++ BoostFileSystem/main.cpp
@@ -3,23 +3,29 @@
 #include <vector>
 #include <algorithm>
 #include <cstring>
-#include <cstdlib>
 #include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
 
 using namespace boost::filesystem;
+const boost::regex txt_filter("\.txt$");
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+
     path p( argc > 1 ? argv[1] : ".");
     std::vector<directory_entry> v; // To save the file names in a vector.
+    std::vector<std::string> matching_files;
 
-    if(is_directory(p))
-    {
+    if(is_directory(p)) {
         copy(directory_iterator(p), directory_iterator(), back_inserter(v));
-        std::cout << p << " is a directory containing:\n";
+        //std::cout << p << " is a directory containing:\n";
 
-        for ( std::vector<directory_entry>::const_iterator it = v.begin(); it != v.end();  ++ it )
+        for ( std::vector<directory_entry>::const_iterator it = v.begin(); it != v.end();  ++it )
         {
+            if(!boost::filesystem::is_regular_file(it->status()))
+                continue;
+
+            boost::smatch watch;
+
             std::cout<< it->path().string() << std::endl;
         }
     }
--- ThreadApplicationCpp14/main.cpp
+++ ThreadApplicationCpp14/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <thread>
 
-using namespace std;
+//using namespace std;
 
 static bool s_Finished = false;
 
-
 void DoWork() {
 
+    using namespace std::literals::chrono_literals;
+
+    std::cout << "Starting thread id=" << std::this_thread::get_id() << std::endl;
+
     while(!s_Finished) {
-        std::cout << "Working..." <<std::endl;
+        std::cout << "Working..." << std::endl;
+        std::this_thread::sleep_for(1s);
     }
 }
 
