9608c33054b395502d469c34e27e43167725de16
Cvetomir Denchev <cvetomirdenchev@gmail.com>
2018-03-21 13:03:12 +0200

    definition of function for retrieving the dates

--- BoostFileSystem/main.cpp
+++ BoostFileSystem/main.cpp
@@ -33,6 +33,8 @@ void show_result(std::vector<path> p) {
 
 }
 
+std::vector<std::string> get_dates();
+
 int main(int argc, char* argv[]) {
 
     if (argc < 3) {
@@ -68,11 +70,9 @@ int main(int argc, char* argv[]) {
 
             if (*file) {
 
-                //std::ifstream *temp = new std::ifstream(matching_files[i].string());
                 std::string *content = new std::string((std::istreambuf_iterator<char>(*file)), std::istreambuf_iterator<char>());
                 boost::iterator_range<std::string::const_iterator> search_range_iterator = boost::ifind_first(*content, search_string);
 
-                //delete temp;
                 delete content;
 
                 if (search_range_iterator) {
